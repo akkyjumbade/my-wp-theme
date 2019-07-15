0512dd17bece05f80824ae5f554abb2958c79fc4
Akshay Jumbade <akkyjumbade@users.noreply.github.com>
2019-07-15 23:04:47 +0530

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ */
+?>
+
+
+<? get_header() ?>
+
+<!-- content » 404 -->
+
+  <div class="content">
+
+    <h1>Error#404</h1>
+
+    <div class="error404">
+      Page could not be found.
+    </div>
+
+  </div>
+
+<? get_footer() ?>
